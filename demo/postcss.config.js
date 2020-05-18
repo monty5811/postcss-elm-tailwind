82596513b35602ed8a1008be8e4a3e73e6e1c262
Phill Sparks <me@phills.me.uk>
2020-05-18 14:49:56 +0100

    Disable postcss-elm-tailwind in production
    It doesn't really hurt anything but there's no need to be rebuilding the
    TLWND elm files again during this stage of the build.

--- demo/postcss.config.js
+++ demo/postcss.config.js
@@ -1,21 +1,24 @@
+const process = require("process");
+const postcssElmTailwind = require("../index.js")({
+  elmFile: "src/TLWND.elm",
+  elmModuleName: "TLWND",
+  prefix: "tw-",
+  formats: {
+    svg: {
+      elmFile: "src/TLWND/Svg.elm",
+      elmModuleName: "TLWND.Svg"
+    },
+    string: {
+      elmFile: "src/TLWND/String.elm",
+      elmModuleName: "TLWND.String"
+    }
+  }
+});
+
 module.exports = {
   plugins: [
     require("tailwindcss"),
-    require("../index.js")({
-      elmFile: "src/TLWND.elm",
-      elmModuleName: "TLWND",
-      prefix: "tw-",
-      formats: {
-        svg: {
-          elmFile: "src/TLWND/Svg.elm",
-          elmModuleName: "TLWND.Svg"
-        },
-        string: {
-          elmFile: "src/TLWND/String.elm",
-          elmModuleName: "TLWND.String"
-        }
-      }
-    }),
+    ...(process.env.NODE_ENV === "production" ? [] : [postcssElmTailwind]),
     require("autoprefixer")
   ]
 };
